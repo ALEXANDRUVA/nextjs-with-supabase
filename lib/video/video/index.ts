37727f604025cf89dd2f9983e5347c9ea4772e8e
ALEXANDRUVA <157848173+ALEXANDRUVA@users.noreply.github.com>
2026-07-23 20:14:39 +0200

    Add video module exports

---
+++ lib/video/video/index.ts
@@ -0,0 +1,15 @@
+export { videoService } from "./service";
+
+export { KlingVideoProvider } from "./providers/kling";
+
+export type {
+  StartVideoGenerationInput,
+  StartVideoGenerationResult,
+  VideoAspectRatio,
+  VideoDurationSeconds,
+  VideoGenerationMode,
+  VideoJobStatus,
+  VideoProvider,
+  VideoProviderName,
+  VideoProviderTaskResult,
+} from "./types";
